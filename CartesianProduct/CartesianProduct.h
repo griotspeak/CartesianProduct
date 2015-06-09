e505d2ec83b3eeaf3452414ab646e6b45dcf6e7d
TJ Usiyan <griotspeak@gmail.com>
2015-06-09 14:29:35 -0400

    Initial Commit

---
+++ CartesianProduct/CartesianProduct.h
@@ -0,0 +1,19 @@
+//
+//  CartesianProduct.h
+//  CartesianProduct
+//
+//  Created by TJ Usiyan on 6/9/15.
+//  Copyright © 2015 Buttons and Lights LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CartesianProduct.
+FOUNDATION_EXPORT double CartesianProductVersionNumber;
+
+//! Project version string for CartesianProduct.
+FOUNDATION_EXPORT const unsigned char CartesianProductVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CartesianProduct/PublicHeader.h>
+
+
